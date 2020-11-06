4ab1934cb096e8d8093c857074e4bb3da8e682a2
brunoportela <13480425+brunoportela@users.noreply.github.com>
2020-11-06 15:49:52 -0800

    Render home, login and register pages

--- app.js
+++ app.js
@@ -7,12 +7,23 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
 
 
 
-
+//Setup port and listen...
 const port = process.env.PORT;
 const localhost = 3000;
 app.listen(port || localhost, function (){
   console.log("connected");
-})
+});
