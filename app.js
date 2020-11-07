f7003c430825d54b82acb8dc661eb1d25740c82a
brunoportela <13480425+brunoportela@users.noreply.github.com>
2020-11-06 16:28:54 -0800

    Level 1 - Username and Password Only. Plain Text.

--- app.js
+++ app.js
@@ -1,29 +1,81 @@
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
+
+//Setup database
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
 
 app.get("/", function(req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req, res) {
-  res.render("login");
-});
+app.route("/login")
+  //GET REQUEST
+  .get(function(req, res) {
+    res.render("login");
+  })
+  //POST REQUEST
+  .post(function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
 
-app.get("/register", function(req, res) {
-  res.render("register");
-});
+    User.findOne({email: username}, function(err, foundUser) {
+      if(err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          if (foundUser.password === password) {
+            res.render("secrets");
+          }
+        }
+      }
+    });
+  });
 
+app.route("/register")
+  //GET REQUEST
+  .get(function(req, res) {
+    res.render("register");
+  })
+  //POST REQUEST
+  .post(function(req, res) {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password
+    });
+
+    newUser.save(function(err) {
+      if (!err) {
+        res.render("secrets");
+      } else {
+        console.log(err);
+      }
+    });
+  });
 
 
 //Setup port and listen...
 const port = process.env.PORT;
 const localhost = 3000;
-app.listen(port || localhost, function (){
+app.listen(port || localhost, function() {
   console.log("connected");
 });
